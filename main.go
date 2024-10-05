551940a0d4e288b11146c16e7951951ab3da45c9
0xakash9 <devakash256@gmail.com>
2024-10-05 22:11:53 +0530

    bit complex concurrency examples involving worker threads

--- main.go
+++ main.go
@@ -18,7 +18,17 @@ func main() {
 
 	fmt.Println("End of program " + <-message)
 
+	// Adding WaitGroup for fibonacci goroutine
+	wg.Add(1)
+	go func() {
+		fibonacci()
+		defer wg.Done() // Calling Done when fibonacci func is finished executing
+	}()
+
+	
 	wg.Wait()
+
+
 }
 
 func Counter(message chan string) {
@@ -26,4 +36,36 @@ func Counter(message chan string) {
 
 	// using channels to send value
 	message <- "Counter"
+}
+
+func fibonacci() {
+	jobs := make(chan int, 100)
+	results := make(chan int, 100)
+	go worker(jobs, results)
+
+	for i := 0; i < 100; i++ {
+		jobs <- i
+	}
+	close(jobs)
+	fmt.Printf("Jobs in channel: %d\n", len(jobs))
+
+	for i := 0; i < 100; i++ {
+		fmt.Println(<-results)
+	}
+
+}
+
+func worker(jobs <-chan int, results chan<- int) {
+	fmt.Println("CALLED")
+	for n := range jobs {
+		results <- fib(n)
+	}
+}
+
+func fib(n int) int {
+	if n <= 1 {
+		return n
+	}
+
+	return fib(n-1) + fib(n-2)
 }
